07015524de8391d13b440ced9389e3197aad4047
WhiteChocolate0 <anniepai931215@gmail.com>
2026-06-07 23:41:04 +0800

    Refine department college filters

--- static/script.js
+++ static/script.js
@@ -25,12 +25,178 @@ let notificationState = {
 };
 let departmentGroups = {};
 let sportActivityOptions = [];
+const UNDERGRAD_COLLEGE_DEPARTMENTS = {
+  "文學院": ["文學院", "中文系", "外文系", "音樂系", "劇藝系"],
+  "理學院": ["理學院", "化學系", "物理系", "生科系", "應數系"],
+  "工學院": ["資工系", "資工全英班", "電機系", "機電系", "材光系", "光電系"],
+  "管理學院": ["管理學院", "企管系", "資管系", "財管系", "國際經營學程", "國際學士學程"],
+  "海洋科學院": ["海科院（學）", "海資系", "海工系", "海科系"],
+  "社會科學院": ["社科院", "政經系", "社會系"],
+  "西灣學院": ["人科學程", "國際學士原民專班"],
+  "醫學院": ["醫學院", "後醫學系", "生醫科技系", "護理系"],
+};
+
+const UNDERGRAD_COLLEGE_ORDER = [
+  "文學院",
+  "理學院",
+  "工學院",
+  "半導體及重點科技研究學院",
+  "管理學院",
+  "國際金融研究學院",
+  "海洋科學院",
+  "社會科學院",
+  "西灣學院",
+  "醫學院",
+  "其他",
+];
+
+const UNDERGRAD_ASSIGNED_DEPARTMENTS = new Set(
+  Object.values(UNDERGRAD_COLLEGE_DEPARTMENTS).flat(),
+);
+
+const MASTER_COLLEGE_DEPARTMENTS = {
+  "文學院": ["中文碩", "外文碩", "音樂碩", "劇藝碩", "哲學碩", "藝管創業所"],
+  "理學院": ["化學碩", "物理碩", "生科碩", "應數碩"],
+  "工學院": [
+    "電機碩",
+    "電機電力碩",
+    "通訊碩",
+    "電信碩程",
+    "機電碩",
+    "資工碩",
+    "資安碩",
+    "材光碩",
+    "光電碩",
+    "環工碩",
+    "前瞻應材",
+    "積體電路碩",
+    "離岸風電碩",
+    "緯創資通產碩",
+    "零組件研究所",
+  ],
+  "半導體學院": ["半導體封測所", "半導體製造碩"],
+  "管理學院": [
+    "管理學院(碩)",
+    "企企管碩",
+    "企醫管碩",
+    "資管碩",
+    "財管碩",
+    "行銷傳播碩",
+    "金融創新產碩",
+    "人管碩",
+    "人管英語碩程",
+  ],
+  "國際金融學院": ["國際資產所", "數位金融所"],
+  "海洋科學院": ["海資碩", "海工碩", "海科碩", "海事碩", "海生保育碩", "海下科技碩"],
+  "社會科學院": ["政治碩", "經濟碩", "社會碩", "公事碩", "亞太碩", "亞太英語碩程", "教育碩", "教人全英碩"],
+  "西灣學院": ["社會創新所"],
+  "醫學院": ["生醫碩", "醫科碩", "精準醫學碩", "生技醫藥碩"],
+};
+
+const MASTER_COLLEGE_ORDER = [
+  "文學院",
+  "理學院",
+  "工學院",
+  "管理學院",
+  "海洋科學院",
+  "社會科學院",
+  "西灣學院",
+  "醫學院",
+  "半導體學院",
+  "國際金融學院",
+  "其他",
+];
+
+const MASTER_ASSIGNED_DEPARTMENTS = new Set(
+  Object.values(MASTER_COLLEGE_DEPARTMENTS).flat(),
+);
+
+const PROFESSIONAL_MASTER_COLLEGE_DEPARTMENTS = {
+  "文學院": [],
+  "理學院": ["生科碩專"],
+  "工學院": ["環工碩專", "資電碩專", "科技偵查碩專"],
+  "管理學院": ["EMBA碩專", "中山同濟EMBA", "人管碩專", "人亞碩專", "財管碩專", "資管碩專", "行傳碩專"],
+  "海洋科學院": [],
+  "社會科學院": ["亞太碩專", "公事碩專", "政治碩專", "經濟碩專", "社會原碩專", "EMPP碩專", "教育碩專"],
+  "西灣學院": [],
+  "醫學院": [],
+};
+
+const DOCTORAL_COLLEGE_DEPARTMENTS = {
+  "文學院": ["中文博", "外文博", "哲學博"],
+  "理學院": ["化學博", "物理博", "生科博", "應數博", "理學博"],
+  "工學院": ["電機博", "通訊博", "機電博", "資工博", "資工資安博", "材光博", "光電博", "環工博"],
+  "管理學院": ["企管博", "企醫管博", "人管博", "財管博", "資管博"],
+  "海洋科學院": ["海資博", "海工博", "海科博", "海科全英博", "海生科技博士學程"],
+  "社會科學院": ["亞太博", "政治博", "公事博", "教育博", "教人全英博"],
+  "西灣學院": [],
+  "醫學院": ["生醫博", "醫科博", "精準醫學博", "生技醫藥博", "臨床醫博學程"],
+  "半導體學院": ["半導體製造博"],
+  "國際金融學院": [],
+};
+
+const PROGRAM_COLLEGE_DEPARTMENTS = {
+  "碩士班": MASTER_COLLEGE_DEPARTMENTS,
+  "碩專班": PROFESSIONAL_MASTER_COLLEGE_DEPARTMENTS,
+  "博士班": DOCTORAL_COLLEGE_DEPARTMENTS,
+};
+
+const PROGRAM_COLLEGE_ORDERS = {
+  "碩士班": MASTER_COLLEGE_ORDER,
+  "碩專班": [
+    "文學院",
+    "理學院",
+    "工學院",
+    "管理學院",
+    "海洋科學院",
+    "社會科學院",
+    "西灣學院",
+    "醫學院",
+    "其他",
+  ],
+  "博士班": [
+    "文學院",
+    "理學院",
+    "工學院",
+    "管理學院",
+    "海洋科學院",
+    "社會科學院",
+    "西灣學院",
+    "醫學院",
+    "半導體學院",
+    "國際金融學院",
+    "其他",
+  ],
+};
+
+const PROGRAM_ASSIGNED_DEPARTMENTS = Object.fromEntries(
+  Object.entries(PROGRAM_COLLEGE_DEPARTMENTS).map(([category, collegeDepartments]) => [
+    category,
+    new Set(Object.values(collegeDepartments).flat()),
+  ]),
+);
+
+
 const DEPARTMENT_SUBFILTER_GROUPS = {
   "跨院選修": (department) => department.startsWith("跨院選修"),
   "博雅": (department) => department.startsWith("博雅"),
   "跨院EAP/ESP": (department) => ["跨院EAP", "跨院ESP"].includes(department),
   "運動健康": (department) => department.startsWith("運動健康") || department.startsWith("運動進階"),
   "英文": (department) => department.startsWith("英文"),
+  ...Object.fromEntries(
+    Object.entries(UNDERGRAD_COLLEGE_DEPARTMENTS).map(([college, departments]) => [
+      college,
+      (department) => departments.includes(department),
+    ]),
+  ),
+  ...Object.fromEntries(
+    Object.entries(PROGRAM_COLLEGE_DEPARTMENTS).flatMap(([category, collegeDepartments]) =>
+      Object.entries(collegeDepartments).map(([college, departments]) => [
+        `${category}:${college}`,
+        (department) => departments.includes(department),
+      ])
+    ),
+  ),
 };
 const LIBERAL_ARTS_ORDER = [
   "博雅向度一",
@@ -41,6 +207,7 @@ const LIBERAL_ARTS_ORDER = [
   "博雅向度六",
 ];
 const ENGLISH_LEVEL_ORDER = ["英文初級", "英文中級", "英文中高級", "英文高級"];
+
 
 // Sample course data (will be replaced with API calls)
 const sampleCourses = [
@@ -838,13 +1005,44 @@ function getDepartmentsForCategory(category) {
 }
 
 function getDepartmentsForGroup(groupName) {
+  const allDepartments = Object.values(departmentGroups)
+    .flat()
+    .map((item) => item.name || item);
+
+  if (groupName === "其他") {
+    return getDepartmentsForCategory("大學部")
+      .filter((department) => !UNDERGRAD_ASSIGNED_DEPARTMENTS.has(department))
+      .sort((a, b) => a.localeCompare(b, "zh-Hant"));
+  }
+
+  const [programCategory, programCollege] = groupName.includes(":")
+    ? groupName.split(":")
+    : ["", ""];
+
+  if (programCollege === "其他" && PROGRAM_ASSIGNED_DEPARTMENTS[programCategory]) {
+    return getDepartmentsForCategory(programCategory)
+      .filter((department) => !PROGRAM_ASSIGNED_DEPARTMENTS[programCategory].has(department))
+      .sort((a, b) => a.localeCompare(b, "zh-Hant"));
+  }
+
+  const availableDepartments = new Set(allDepartments);
+  if (UNDERGRAD_COLLEGE_DEPARTMENTS[groupName]) {
+    return UNDERGRAD_COLLEGE_DEPARTMENTS[groupName].filter((department) =>
+      availableDepartments.has(department)
+    );
+  }
+
+  const collegeDepartments = PROGRAM_COLLEGE_DEPARTMENTS[programCategory]?.[programCollege];
+  if (collegeDepartments) {
+    return collegeDepartments.filter((department) =>
+      availableDepartments.has(department)
+    );
+  }
+
   const matcher = DEPARTMENT_SUBFILTER_GROUPS[groupName];
   if (!matcher) return [];
 
-  const departments = Object.values(departmentGroups)
-    .flat()
-    .map((item) => item.name || item)
-    .filter((department) => matcher(department));
+  const departments = allDepartments.filter((department) => matcher(department));
 
   if (groupName === "博雅") {
     return [...departments].sort((a, b) => {
@@ -944,6 +1142,20 @@ function getDepartmentFilterOptions(category) {
     ];
   }
 
+  if (category === "大學部") {
+    return UNDERGRAD_COLLEGE_ORDER.map((college) => ({
+      label: college,
+      group: college,
+    })).filter((option) => getDepartmentsForGroup(option.group).length > 0);
+  }
+
+  if (PROGRAM_COLLEGE_ORDERS[category]) {
+    return PROGRAM_COLLEGE_ORDERS[category].map((college) => ({
+      label: college,
+      group: `${category}:${college}`,
+    })).filter((option) => getDepartmentsForGroup(option.group).length > 0);
+  }
+
   if (category === "校際") {
     return [
       { label: "大學部", value: "校際(學士班)" },
@@ -970,9 +1182,10 @@ function renderDepartmentSubFilter(groupName, targetButton) {
   const select = document.createElement("select");
   select.className = "department-subfilter-select";
   select.id = "deptSubFilterSelect";
+  const displayLabel = targetButton.dataset.label || groupName;
   select.dataset.group = groupName;
-  select.dataset.label = targetButton.dataset.label || groupName;
-  select.innerHTML = `<option value="">${escapeHtml(groupName)}: All</option>${options}`;
+  select.dataset.label = displayLabel;
+  select.innerHTML = `<option value="">${escapeHtml(displayLabel)}: All</option>${options}`;
   select.value = "";
   select.addEventListener("change", filterCourses);
   targetButton.replaceWith(select);
